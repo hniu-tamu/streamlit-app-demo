d7364377c37d13b12d2e4335a16f4e61cb94d62e
hniu <hniu@tamu.edu>
2023-05-09 11:13:32 -0500

    added a few pages

---
+++ pages/About.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('About')
+
+st.write('This is a demo app to show how to build Streamlit apps.')
+
+st.write('This is a demo app to show how to build Streamlit apps.')
