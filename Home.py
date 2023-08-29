bcf37e0c58613785f402c497ed7f30dcaf36a3be
hniu <hniu@tamu.edu>
2023-08-29 15:33:36 -0500

    added a st map

--- Home.py
+++ Home.py
@@ -2,39 +2,63 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-st.title('Uber pickups in NYC')
+st.title('Cotton Features and Yield in Lubbock, TX')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_COLUMN = 'yield'
+# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+#             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_FILE_PATH = 'my_cotton_yield_data.csv'
 
 @st.cache_data
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    data = pd.read_csv(DATA_FILE_PATH, nrows=nrows)
     return data
 
 data_load_state = st.text('Loading data...')
-data = load_data(10000)
+data = load_data(96) # because we only have 96 rows
 data_load_state.text("Done! (using st.cache_data)")
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+st.subheader('Yield distribution')
+sns.histplot(data[DATA_COLUMN], kde=True)
+plt.xlabel('Yield')
+plt.ylabel('Frequency')
+plt.title('Yield Distribution')
+st.pyplot(plt)
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+# A demo, change the lat, lon, in the future.
+lat = 33.5934
+start_point = -101.9029
+end_point = -101.9014
+num_points = 96
+
+points = np.linspace(start_point, end_point, num_points)
+
+data['lon'] = points
+data['lat'] = np.full(num_points, lat)
+data['size'] = data['yield']/70
+
+def map_irrigation_color(irrigation_value):
+    if irrigation_value == 0:
+        return '#FF0000' # red
+    elif irrigation_value == 0.1:
+        return '#00FF00' # green
+    elif irrigation_value == 0.2:
+        return '#0000FF' # blue
+    else: 
+        return '#FFFF00'  # Yellow
+
+# Apply the custom function to create a new column for color
+data['color'] = data['irrigation'].apply(map_irrigation_color)
+
+st.map(data, size = 'size', color = 'color') # filtered_data
 
 # image = Image.open('sunrise.jpg')
 image = 'https://i.gifer.com/4j.gif'
