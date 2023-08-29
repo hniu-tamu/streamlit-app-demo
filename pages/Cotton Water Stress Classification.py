518998f2ee7c1351463e4ffdb5f55023b0af678f
hniu <hniu@tamu.edu>
2023-08-29 12:58:41 -0500

    added model file

--- pages/Cotton Water Stress Classification.py
+++ pages/Cotton Water Stress Classification.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import tensorflow as tf
-import streamlit as st
 
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('pages/my_model2.hdf5')
+  model=tf.keras.models.load_model('pages/my_cotton_classification_model.hdf5')
   return model
 with st.spinner('Model is being loaded..'):
   model=load_model()
@@ -15,7 +14,7 @@ st.write("""
          """
          )
 
-file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png"])
+file = st.file_uploader("Please upload an brain scan file", type=["TIF"])
 import cv2
 from PIL import Image, ImageOps
 import numpy as np
@@ -23,19 +22,20 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 def import_and_predict(image_data, model):
     
-        size = (180,180)    
+        size = (64,64)
+        x = []    
+        # full_size_image = image_data
+        # x.append(cv2.resize(full_size_image, size, interpolation=cv2.INTER_CUBIC))
+        # x = np.array(x)
         image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
         image = np.asarray(image)
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
-        
         img_reshape = img[np.newaxis,...]
-    
         prediction = model.predict(img_reshape)
         
         return prediction
 
-class_names=['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
+class_names=['rainfed', 'fully irrigated', 'percent deficit', 'time delay']
 if file is None:
     st.text("Please upload an image file")
 else:
